a4d7745144459db84cf9910f091427c407265909
Deanosim <deanosim@protonmail.com>
2019-12-10 03:13:32 +1100

    change from mycog to simplealert

--- simplealert/simplealert.py
+++ simplealert/simplealert.py
@@ -1,10 +1,10 @@
 from redbot.core import commands
 
-class Mycog(commands.Cog):
+class simplealert(commands.Cog):
     """My custom cog"""
 
     @commands.command()
-    async def mycom(self, ctx):
+    async def simplealert(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
+        await ctx.send("World Domination is near!")
