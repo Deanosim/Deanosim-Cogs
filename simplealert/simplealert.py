7c49b2ed97fbdfe341a67d1625df4ec73e958cfe
Deanosim <deanosim+git@pm.me>
2019-12-10 04:42:08 +1100

    add date and time to command output

--- simplealert/simplealert.py
+++ simplealert/simplealert.py
@@ -1,14 +1,18 @@
 import asyncio
 import time
+import datetime
 
 import discord
 from redbot.core import commands
+from datetime import datetime
 
 class simplealert(commands.Cog):
     """My custom cog"""
+
 
     @commands.command()
     async def simplealert(self, ctx):
         """This does stuff!"""
         # Your code will go here
         await ctx.send("World Domination is near!")
+        await ctx.send(datetime.strftime("Current Date and time is: %Y-%m-%d %H:%M:%S"))
