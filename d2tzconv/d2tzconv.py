0a18444084f6bc43a5850614313e5d84ae029b95
Deanosim <4834976+Deanosim@users.noreply.github.com>
2024-05-01 02:06:36 +1000

    add code to time current time, convert it to epoch and then add the discord timestamp tags

--- d2tzconv/d2tzconv.py
+++ d2tzconv/d2tzconv.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 from redbot.core import commands
 
 class d2tzconv(commands.Cog):
@@ -7,5 +8,18 @@ class d2tzconv(commands.Cog):
     @commands.command()
     async def d2tzconv(self, ctx):
         """This does stuff!"""
+
+            # Get the current datetime
+        py_dt = datetime.datetime.now()
+
+        # Convert the datetime to an epoch timestamp (rounded to the nearest second)
+        epoch = round(py_dt.timestamp())
+
+        # Create the Discord timestamp string
+        disc_dt = f"<t:{epoch}:R>"
+
         # Your code will go here
         await ctx.send("This might actually do something one day.")
+        await ctx.send(disc_dt)
+        
+
