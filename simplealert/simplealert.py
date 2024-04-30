2571546fb230d72397218e22d2f802597715bee1
Deanosim <4834976+Deanosim@users.noreply.github.com>
2024-05-01 01:01:38 +1000

    Remove all the broken timedate code

--- simplealert/simplealert.py
+++ simplealert/simplealert.py
@@ -1,20 +1,12 @@
-import asyncio
-import time
-import datetime
 
 import discord
 from redbot.core import commands
-from datetime import datetime
 
 class simplealert(commands.Cog):
     """My custom cog"""
-
 
     @commands.command()
     async def simplealert(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        fmt = "**%Y-%m-%d %H:%M:%S**"
-        time = time.strftime(fmt)
         await ctx.send("World Domination is near!")
-        await ctx.send(f"{str(time)}")
